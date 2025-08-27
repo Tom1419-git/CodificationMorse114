3f59fd93b9b4b4a6f595ba8b36c34e20fbaa7946
Thomas Mayoraz <thomas.mayoraz@eduvaud.ch>
2025-08-27 11:34:39 +0200

    Création du titre

--- CodificationMorseCode/CodificationMorseCode/Program.cs
+++ CodificationMorseCode/CodificationMorseCode/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace CodificationMorseCode
+﻿/*
+ ETML
+ Thomas Mayoraz
+ Created: 27.08.2025
+ Last modified : 27.08.2025
+ */
+
+namespace CodificationMorseCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AfficherTitreASCII();
+
+        }
+        /// <summary>
+        /// Affiche le titre ASCII art du programme
+        /// </summary>
+        private static void AfficherTitreASCII()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\t\t\t╔══════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("\t\t\t║  ███╗   ███╗ ██████╗ ██████╗ ███████╗███████╗    ██████╗ ██████╗ ██████╗ ███████╗    ║");
+            Console.WriteLine("\t\t\t║  ████╗ ████║██╔═══██╗██╔══██╗██╔════╝██╔════╝   ██╔════╝██╔═══██╗██╔══██╗██╔════╝    ║");
+            Console.WriteLine("\t\t\t║  ██╔████╔██║██║   ██║██████╔╝███████╗█████╗     ██║     ██║   ██║██║  ██║█████╗      ║");
+            Console.WriteLine("\t\t\t║  ██║╚██╔╝██║██║   ██║██╔══██╗╚════██║██╔══╝     ██║     ██║   ██║██║  ██║██╔══╝      ║");
+            Console.WriteLine("\t\t\t║  ██║ ╚═╝ ██║╚██████╔╝██║  ██║███████║███████╗   ╚██████╗╚██████╔╝██████╔╝███████╗    ║");
+            Console.WriteLine("\t\t\t║  ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝    ║");
+            Console.WriteLine("\t\t\t║                                                                                      ║");
+            Console.WriteLine("\t\t\t║                            CRÉÉ PAR : Thomas MAYORAZ (MIN2B)                         ║");
+            Console.WriteLine("\t\t\t║                                  PROJET : C114 - 2025                                ║");
+            Console.WriteLine("\t\t\t║                                                                                      ║");
+            Console.WriteLine("\t\t\t╚══════════════════════════════════════════════════════════════════════════════════════╝");
+            Console.WriteLine();
+            Console.ResetColor();
+
+           
         }
     }
 }
